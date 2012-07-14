e3a3efcb6a49e573d80a9ee2214d95dc278de0fb
Vasya Novikov <vn91@yaaaaandex.ru>
2012-07-14 17:17:23 +0400

    minor update

--- src/main/scala/code/comet/GamesStart.scala
+++ src/main/scala/code/comet/GamesStart.scala
@@ -6,11 +6,11 @@ import java.util.Date
 import ru.ya.vn91.robotour.Constants._
 import ru.ya.vn91.robotour.Constants
 
-class TimeStart extends CometActor with CometListener {
+class GamesStart extends CometActor with CometListener {
 	private var time = 0L
 	def registerWith = TimeStartSingleton
 	override def lowPriority = {
 		case newTime: Long => time = newTime; reRender()
 	}
-	def render = "*" #> (if (time > 0) timeLongToString(time - registrationLength) else "undefined yet")
+	def render = "*" #> (if (time > 0) timeLongToString(time) else "undefined yet")
 }
