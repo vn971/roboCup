d2c02cc8f91c4768c49d5ef42f31c6a12539b1c3
Vasya Novikov <vn91@yaaaaandex.ru>
2013-04-19 19:41:03 +0400

    don't show registered list until registration starts

--- src/main/scala/code/comet/RegisteredList.scala
+++ src/main/scala/code/comet/RegisteredList.scala
@@ -1,12 +1,12 @@
-package code
-package comet
+package code.comet
 
 import net.liftweb.http._
+import net.liftweb.util.ClearNodes
 
 /** The screen real estate on the browser will be represented
- *  by this component.  When the component changes on the server
- *  the changes are automatically reflected in the browser.
- */
+	* by this component.  When the component changes on the server
+	* the changes are automatically reflected in the browser.
+	*/
 class RegisteredList extends CometActor with CometListener {
 	private var msgs: Vector[String] = Vector() // private state
 
@@ -16,5 +16,8 @@ class RegisteredList extends CometActor with CometListener {
 		case v: Vector[_] => msgs = v.asInstanceOf[Vector[String]]; reRender()
 	}
 
-	def render = "li *" #> msgs
+	def render = if (msgs.isEmpty)
+		ClearNodes
+	else
+		"li *" #> msgs
 }
