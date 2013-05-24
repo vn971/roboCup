669d96b3455f3ee39d891f492f96b5e93d77458c
Vasya Novikov <n1dr+git9270@yaaaaandex.com>
2013-05-24 23:21:45 +0400

    less verbose FromZagram

--- src/main/scala/ru/ya/vn91/robotour/FromZagram.scala
+++ src/main/scala/ru/ya/vn91/robotour/FromZagram.scala
@@ -30,60 +30,64 @@ class FromZagram extends Actor with Loggable {
 			val urlAsString = s"http://zagram.org/a.kropki?idGracza=$idGracza&co=getMsg&msgNo=$messageCount&wiad=x"
 			val text = getLinkContent(urlAsString)
 			logger.trace(s"HTTP GET $urlAsString => $text")
-			for (line <- text.split('/').filter(_.length > 0)) try {
-				val dotSplit = line.substring(1).split('.')
-				if (line.startsWith("ca")) {
-					// chat
-					val innerSplit = line.split("\\.", 4)
-					val nick = innerSplit(1)
-					val chatMessage = getServerDecoded(innerSplit(3))
-					// ChatServer ! ChatMessage(chatMessage, time, "zagram", nick)
-					if (
-						chatMessage.startsWith("!register") ||
-								chatMessage.startsWith("!register") ||
-								chatMessage.startsWith("!register")) {
-						playerSet.get(nick) match {
-							case None =>
-								logger.info(s"failed to register: no userinfo for $nick")
-								context.parent ! MessageToZagram(s"Sorry, could not find zagram rank for $nick")
-							case Some(info) =>
-								logger.debug("tried to register: " + nick)
-								context.parent ! TryRegister(info)
+			if (text.startsWith("sd") && text.endsWith("end")) {
+				for (line <- text.split('/').filter(_.length > 0)) try {
+					val dotSplit = line.substring(1).split('.')
+					if (line.startsWith("ca")) {
+						// chat
+						val innerSplit = line.split("\\.", 4)
+						val nick = innerSplit(1)
+						val chatMessage = getServerDecoded(innerSplit(3))
+						// ChatServer ! ChatMessage(chatMessage, time, "zagram", nick)
+						if (
+							chatMessage.startsWith("!register") ||
+									chatMessage.startsWith("!register") ||
+									chatMessage.startsWith("!register")) {
+							playerSet.get(nick) match {
+								case None =>
+									logger.info(s"failed to register: no userinfo for $nick")
+									context.parent ! MessageToZagram(s"Sorry, could not find zagram rank for $nick")
+								case Some(info) =>
+									logger.debug("tried to register: " + nick)
+									context.parent ! TryRegister(info)
+							}
 						}
+					} else if (line.startsWith("m")) {
+						// message count
+						messageCount = line.split('.')(1).toLong
+					} else if (line startsWith "x") {
+						// game state info
+						val sgfResult = dotSplit(2)
+						val first = gameSet(dotSplit(0)).first
+						val second = gameSet(dotSplit(0)).second
+						if (sgfResult startsWith "B+") {
+							context.parent ! GameWon(first, second)
+						} else if ((sgfResult startsWith "W+") || (sgfResult == "Void")) {
+							context.parent ! GameWon(second, first)
+						} else if (sgfResult == "0") {
+							context.parent ! GameDraw(first, second)
+						} // else still playing
+					} else if (line startsWith "d") {
+						val tableN = dotSplit(0).toInt
+						val first = getServerDecoded(dotSplit(dotSplit.length - 2))
+						val second = getServerDecoded(dotSplit(dotSplit.length - 1))
+						gameSet += tableN.toString -> GameInfo(first, second)
+					} else if (line startsWith "i") {
+						// player info
+						val player = dotSplit(0)
+						val (rating, wins, losses, draws) =
+							if (dotSplit.length >= 8)
+								(dotSplit(4).toInt, dotSplit(5).toInt, dotSplit(7).toInt, dotSplit(6).toInt)
+							else
+								(0, 0, 0, 0)
+						playerSet += player -> PlayerInfo(player, rating, wins, losses, draws)
 					}
-				} else if (line.startsWith("m")) {
-					// message count
-					messageCount = line.split('.')(1).toLong
-				} else if (line startsWith "x") {
-					// game state info
-					val sgfResult = dotSplit(2)
-					val first = gameSet(dotSplit(0)).first
-					val second = gameSet(dotSplit(0)).second
-					if (sgfResult startsWith "B+") {
-						context.parent ! GameWon(first, second)
-					} else if ((sgfResult startsWith "W+") || (sgfResult == "Void")) {
-						context.parent ! GameWon(second, first)
-					} else if (sgfResult == "0") {
-						context.parent ! GameDraw(first, second)
-					} // else still playing
-				} else if (line startsWith "d") {
-					val tableN = dotSplit(0).toInt
-					val first = getServerDecoded(dotSplit(dotSplit.length - 2))
-					val second = getServerDecoded(dotSplit(dotSplit.length - 1))
-					gameSet += tableN.toString -> GameInfo(first, second)
-				} else if (line startsWith "i") {
-					// player info
-					val player = dotSplit(0)
-					val (rating, wins, losses, draws) =
-						if (dotSplit.length >= 8)
-							(dotSplit(4).toInt, dotSplit(5).toInt, dotSplit(7).toInt, dotSplit(6).toInt)
-						else
-							(0, 0, 0, 0)
-					playerSet += player -> PlayerInfo(player, rating, wins, losses, draws)
+				} catch {
+					case e: Exception =>
+						logger.warn(s"error processing line: $line, exception: $e")
 				}
-			} catch {
-				case e: Exception =>
-					logger.warn(s"error processing line: $line, exception: $e")
+			} else {
+				logger.info("incorrect server response")
 			}
 		} catch {
 			case e: Exception =>
