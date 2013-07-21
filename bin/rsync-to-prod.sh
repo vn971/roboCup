7638bcf87556398155dad7bc18017873e76a342a
Vasya Novikov <n1dr+git9270@yaaaaandex.com>
2013-07-22 01:29:47 +0400

    update rsync-to-prod.sh

--- bin/rsync-to-prod.sh
+++ bin/rsync-to-prod.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -e
 
-sbt package
+cd `dirname $0`/../
+
+./bin/sbt package
 
 ssh pointsgame.net service tomcat7 stop
-
-rsync -aruvz --progress --delete target/webapp/ pointsgame.net:/var/lib/tomcat7/webapps/tournament/
-
+rsync -aruvz --progress --delete target/webapp/ lenovo.pointsgame.net:/var/lib/tomcat7/webapps/tournament/
 ssh pointsgame.net service tomcat7 start
