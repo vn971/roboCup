ff6927e35ddedbfc4bebee54b53ae6431ab074c1
Vasya Novikov <n1dr+git9270@yandex.com>
2015-08-16 11:33:54 +0200

    minor changes: knock-out tree printer

--- src/main/scala/ru/ya/vn91/robotour/GameNode.scala
+++ src/main/scala/ru/ya/vn91/robotour/GameNode.scala
@@ -3,18 +3,18 @@ package ru.ya.vn91.robotour
 case class GameNode(name: String, children: GameNode*)
 
 object GameNode {
-	def toTree(node: GameNode): String = toTree(node, "", "")
+	def toTree(node: GameNode): String = toTree(node, "", "").mkString("\n")
 
-	private def toTree(node: GameNode, prefix: String, childrenPrefix: String): String = {
+	private def toTree(node: GameNode, prefix: String, childrenPrefix: String): Seq[String] = {
 		val firstLine = prefix + node.name
 
-		val headChildren = node.children.dropRight(1).map { child =>
+		val headChildren = node.children.dropRight(1).flatMap { child =>
 			toTree(child, childrenPrefix + "├── ", childrenPrefix + "│   ")
 		}
-		val lastChild = node.children.takeRight(1).map { child =>
+		val lastChild = node.children.takeRight(1).flatMap { child =>
 			toTree(child, childrenPrefix + "└── ", childrenPrefix + "    ")
 		}
-		(firstLine +: headChildren ++: lastChild).mkString("\n")
+		firstLine +: headChildren ++: lastChild
 	}
 
 }
